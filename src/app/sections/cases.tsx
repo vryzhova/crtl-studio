e62e721a0314a029a2b251a98ee96d06bb1264d4
Valeria Ryzhova <42956604+vryzhova@users.noreply.github.com>
2025-07-14 00:13:21 +0300

    fix animation

--- src/app/sections/cases.tsx
+++ src/app/sections/cases.tsx
@@ -33,9 +33,10 @@ export const Cases = () => {
 
   const initialTextRef = useRef<HTMLDivElement>(null);
   const rightPanelRef = useRef<HTMLDivElement>(null);
-  const caseNumbersRef = useRef<HTMLSpanElement>(null);
-  const packedTextRef = useRef<HTMLSpanElement>(null);
+  const initialTextContainerRef = useRef<HTMLDivElement>(null);
+  const caseNumbersRef = useRef<HTMLHeadingElement>(null);
   const businessesTextRef = useRef<HTMLHeadingElement>(null);
+  const caseInfoRef = useRef<HTMLDivElement>(null);
 
   const [activeCase, setActiveCase] = useState<Case | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
@@ -44,22 +45,21 @@ export const Cases = () => {
 
   const cases = useMemo(() => getCases(t), [t]);
   const isDesktop = typeof window !== 'undefined' && window.innerWidth > 1024;
-  console.log(isDesktop);
-
   const textStyle =
-    'text-2xl md:text-4xl px-5 lg:px-0 lg:text-end font-bold lg:pb-20 mb-2 bg-gradient-to-b from-white lg:from-black to-gray-gradient bg-clip-text text-transparent';
+    'text-center md:text-left text-2xl md:text-4xl px-5 lg:px-0 lg:text-end font-bold bg-gradient-to-b from-white lg:from-black to-gray-gradient bg-clip-text text-transparent';
 
   useEffect(() => {
     if (!containerRef.current || containerAnimationDone || !contentRef.current) return;
-    gsap.set(initialTextRef.current, { opacity: 1 });
-    gsap.set(contentRef.current, { opacity: 0 });
 
     if (isDesktop) {
-      // Set initial state for desktop
+      gsap.set(initialTextRef.current, { opacity: 1 });
+      gsap.set(contentRef.current, { opacity: 0 });
       gsap.set(rightPanelRef.current, { opacity: 1, x: '100%' });
     } else {
-      // gsap.set(caseNumbersRef.current, { opacity: 0, y: 20 });
-      // gsap.set(packedTextRef.current, { opacity: 0, y: 20 });
+      gsap.set(contentRef.current, { opacity: 1 });
+      gsap.set(wrapperRef.current, { opacity: 0 });
+      gsap.set(caseInfoRef.current, { opacity: 0 });
+      gsap.set(caseNumbersRef.current, { opacity: 0 });
     }
 
     const observer = new IntersectionObserver(
@@ -83,25 +83,26 @@ export const Cases = () => {
 
       if (isDesktop) {
         timeline
-          .to({}, { duration: 1 })
+          .to({}, { duration: 2 })
           .to(containerRef.current, { duration: 1 })
           .to(initialTextRef.current, { x: -350, duration: 1 }, '-=0.8')
           .to(rightPanelRef.current, { opacity: 1, x: '0%', duration: 0.8 }, '-=0.8')
-          .to(rightPanelRef.current, { opacity: 1, duration: 0.8, backgroundColor: '#141414' })
-          .to(initialTextRef.current, { display: 'none', duration: 1 })
+          .to(initialTextRef.current, { opacity: 0, duration: 1 })
+          .to(rightPanelRef.current, { opacity: 0, duration: 0.8 })
+          .add('simultaneous')
+          .to(initialTextRef.current, { display: 'none', duration: 0.5 }, 'simultaneous')
+          .to(rightPanelRef.current, { display: 'none', duration: 0.5 }, 'simultaneous')
+          .to(initialTextContainerRef.current, { display: 'none', duration: 0.5 }, 'simultaneous')
           .to(contentRef.current, { opacity: 1, duration: 0.8 });
-
-        console.log('here1');
 
         setContainerAnimationDone(true);
       } else {
         timeline
           .to({}, { duration: 1 })
-          .to(containerRef.current, { duration: 1 })
-          .to(initialTextRef.current, { opacity: 0, duration: 1 })
-          .to(contentRef.current, { opacity: 1, duration: 0.8 });
-
-        console.log('here');
+          .to(caseNumbersRef.current, { opacity: 1, duration: 1 })
+          .add('simultaneous')
+          .to(wrapperRef.current, { opacity: 1, duration: 0.8 }, 'simultaneous')
+          .to(caseInfoRef.current, { opacity: 1, duration: 0.8 }, 'simultaneous');
 
         setContainerAnimationDone(true);
       }
@@ -124,7 +125,7 @@ export const Cases = () => {
 
     const totalWidth = wrapper.scrollWidth;
     const scrollDistance = totalWidth - window.innerWidth;
-    const start = isDesktop ? 'top top+=20%' : 'top top';
+    const start = isDesktop ? 'top top+=10%' : 'top top';
 
     // Высота wrapper — длина прокрутки + высота окна,
     // чтобы ScrollTrigger мог прокрутить весь горизонтальный контент
@@ -198,49 +199,52 @@ export const Cases = () => {
   return (
     <section id="cases" ref={containerRef} className="relative overflow-hidden bg-black z-10">
       {/* Initial title */}
-      <h2
-        ref={initialTextRef}
-        className="lg:absolute lg:top-1/2 lg:left-1/2 text-start lg:text-center py-10 lg:pb-20 px-5 text-white z-20 font-inter-tight font-bold leading-tight lg:text-4xl 2xl:text-6xl md:text-3xl text-[28px]"
-      >
-        {t('cases.title')}
-      </h2>
+      {/*<div className="relative h-screen w-full" ref={initialTextContainerRef}>*/}
 
-      {/*<div className="flex flex-col lg:hidden">*/}
-      {/*  <span ref={caseNumbersRef} className={textStyle}>*/}
-      {/*    8 кейсов*/}
-      {/*  </span>*/}
-      {/*  <span ref={packedTextRef} className={textStyle}>*/}
-      {/*    8 упакованных бизнесов*/}
-      {/*  </span>*/}
-      {/*</div>*/}
+      <div
+        ref={initialTextContainerRef}
+        className="hidden lg:flex absolute w-screen h-screen flex-col items-center justify-center z-20"
+      >
+        <div ref={initialTextRef} className="flex flex-col lg:items-end justify-center items-center">
+          <h2 className="text-start lg:text-center z-20 font-inter-tight font-bold leading-tight lg:text-4xl 2xl:text-6xl md:text-3xl text-[28px] bg-gradient-to-b from-white to-text-grad-dbg bg-clip-text text-transparent">
+            {t('cases.title')}
+          </h2>
+        </div>
+      </div>
 
       <div
         ref={rightPanelRef}
-        className="hidden lg:flex absolute w-1/2 h-full right-0 flex-col items-center justify-center pl-12 bg-white"
+        className="hidden lg:flex absolute w-1/2 h-screen right-0 flex-col items-center justify-center pl-12 bg-white z-20"
       >
-        <div className="flex flex-col lg:items-end">
-          <h2 ref={businessesTextRef} className={textStyle}>
+        <div className="flex flex-col lg:items-end items-center">
+          <h2 ref={businessesTextRef} className={`${textStyle} whitespace-pre-line`}>
             {t('cases.subtitle')}
           </h2>
         </div>
       </div>
 
-      <div id="gallery" ref={contentRef} className="relative h-screen overflow-hidden z-10">
+      <div
+        id="gallery"
+        ref={contentRef}
+        className="relative h-screen overflow-hidden z-10 flex flex-col justify-center"
+      >
         {/* Header */}
-        <div className="absolute top-[-50px] left-0 w-full justify-between flex items-start px-10 pt-12 z-20 pointer-events-none display-none lg:display">
-          <h2 className="text-2xl md:text-3xl font-bold text-[#CFCFCF]">Наши кейсы</h2>
-          <div className="text-right">
-            <div className="text-2xl md:text-3xl font-bold text-white">8 кейсов</div>
-            <div className="text-xl md:text-2xl font-bold text-[#979797] leading-tight">
-              8 упакованных
-              <br />
-              бизнесов
-            </div>
+        <div className="absolute top-[-30px] left-0 w-full flex lg:justify-between flex-col lg:flex-row items-start lg:px-25 px-4 gap-5 pt-12 z-20 pointer-events-none">
+          <h2 className="text-[28px] sm:text-[44px] font-bold bg-gradient-to-b from-white to-text-grad-dbg bg-clip-text text-transparent">
+            {t('cases.title')}
+          </h2>
+          <div className="lg:text-right">
+            <span
+              ref={caseNumbersRef}
+              className="whitespace-pre-line sm:text-[44px] text-[28px] font-bold bg-gradient-to-b from-white to-text-grad-dbg bg-clip-text text-transparent"
+            >
+              {t('cases.subtitle')}
+            </span>
           </div>
         </div>
 
         {/* Horizontal scroll */}
-        <div ref={wrapperRef} className="flex items-center" style={{ width: `${cases.length * 80}vw` }}>
+        <div ref={wrapperRef} className="flex lg:mt-0 mt-30 items-center" style={{ width: `${cases.length * 60}vw` }}>
           {cases.map((item, index) => {
             const setRef = (el: HTMLDivElement) => (itemRefs.current[index] = el);
             return (
@@ -248,7 +252,7 @@ export const Cases = () => {
                 key={item.id}
                 // @ts-ignore
                 ref={setRef}
-                className="w-[80vw] lg:h-[70vh] flex items-center justify-center p-2.5 cursor-pointer"
+                className="w-[60vw] sm:h-[55vh] h-[20vh] flex items-center justify-center p-2.5 cursor-pointer"
                 onClick={() => openGallery(item)}
               >
                 <Image
@@ -265,10 +269,11 @@ export const Cases = () => {
 
         {/* Case info + arrow */}
         <div
-          className="sticky bottom-10 right-10 flex gap-2 h-[70px] z-30"
+          ref={caseInfoRef}
+          className="sticky mt-6.5 lg:right-20 flex gap-2 h-[70px] z-30"
           style={{ position: 'sticky', marginLeft: 'auto', width: 'fit-content' }}
         >
-          <div className="p-6 flex items-center justify-between bg-lime-default rounded-md w-fulllg:w-[540px] cursor-default select-none">
+          <div className="p-6 flex items-center justify-between bg-lime-default rounded-md lg:w-[540px] w-[250px] cursor-default select-none">
             <span className="font-bold text-black truncate">{cases[activeIndex]?.title}</span>
             <span className="px-3 text-black">{cases[activeIndex]?.year}</span>
           </div>
@@ -279,13 +284,13 @@ export const Cases = () => {
             <svg width="70" height="70" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect x="2" y="2" width="56" height="56" rx="12" fill="#D1F48C" />
               <rect x="2" y="2" width="56" height="56" rx="12" stroke="none" />
-              <path d="M15 15 V25 M15 15 H25" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M45 15 V25 M45 15 H35" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M15 45 V35 M15 45 H25" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M45 45 V35 M45 45 H35" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M23 23 L37 37" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M37 37 H30" stroke="#222" strokeWidth="3" strokeLinecap="round" />
-              <path d="M37 37 V30" stroke="#222" strokeWidth="3" strokeLinecap="round" />
+              <path d="M10 10 V18 M10 10 H18" stroke="#222" strokeWidth="1.5" strokeLinecap="round" />
+              <path d="M50 10 V18 M50 10 H42" stroke="#222" strokeWidth="1.5" strokeLinecap="round" />
+              <path d="M10 50 V42 M10 50 H18" stroke="#222" strokeWidth="1.5" strokeLinecap="round" />
+              <path d="M50 50 V42 M50 50 H42" stroke="#222" strokeWidth="1.5" strokeLinecap="round" />
+              <path d="M23 23 L37 37" stroke="#222" strokeWidth="1" strokeLinecap="round" />
+              <path d="M37 37 H30" stroke="#222" strokeWidth="1" strokeLinecap="round" />
+              <path d="M37 37 V30" stroke="#222" strokeWidth="1" strokeLinecap="round" />
             </svg>
           </div>
         </div>
